9c708a1080c1d5f5cdf27cd5ab7fb7112a12c88f
Ignacio Fernandez <ifernandezcerro@gmail.com>
2023-05-07 23:01:24 +0200

    diccionario

--- src/components/Acordeon/Acordeon.js
+++ src/components/Acordeon/Acordeon.js
@@ -1,20 +1,46 @@
-import transformArray from "@/utils/transformArray";
 import React, { useState } from "react";
 import Accordion from "react-bootstrap/Accordion";
+import Button from "react-bootstrap/Button";
+import transformArray from "@/utils/transformArray";
 
-const Acordeon = ({data}) => {
+const Acordeon = ({ data }) => {
+  const dataFormat = transformArray(data);
+  const [sortedData, setSortedData] = useState(dataFormat);
+  const [isSorted, setIsSorted] = useState(false);
 
-  const dataFormat = transformArray(data)
-  console.log(dataFormat)
+  const handleSort = () => {
+    if (!isSorted) {
+      const sorted = [...sortedData].sort((a, b) => a.prompt.localeCompare(b.prompt));
+      setSortedData(sorted);
+    } else {
+      setSortedData(dataFormat);
+    }
+    setIsSorted(!isSorted);
+  };
+
   return (
-    <Accordion>
-      {dataFormat.map((item, index) => (
-        <Accordion.Item key={index} eventKey={index.toString()}>
-          <Accordion.Header>{item.prompt}</Accordion.Header>
-          <Accordion.Body>{item.response}</Accordion.Body>
-        </Accordion.Item>
-      ))}
-    </Accordion>
+    <>
+      <div className="d-flex justify-content-start mb-3">
+        <Button variant="primary" onClick={handleSort}>
+          {isSorted ? "Desordenar" : "Ordenar alfabéticamente"}
+        </Button>
+      </div>
+      <Accordion>
+        {sortedData.map((item, index) => (
+          <Accordion.Item key={index} eventKey={index.toString()}>
+            <Accordion.Header>{item.prompt}</Accordion.Header>
+            <Accordion.Body>
+              {item.response.split("\n").map((line, idx) => (
+                <React.Fragment key={idx}>
+                  {line}
+                  <br />
+                </React.Fragment>
+              ))}
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </>
   );
 };
 
--- src/utils/transformArray.js
+++ src/utils/transformArray.js
@@ -3,13 +3,20 @@ export default function transformArray(inputArray) {
 
   for (let i = 0; i < inputArray.length; i += 2) {
     if (inputArray[i].role === "Prompt" && inputArray[i + 1]?.role === "Response") {
-      outputArray.push({
-        prompt: inputArray[i].say,
-        response: inputArray[i + 1].say,
-      });
+      const response = inputArray[i + 1].say
+        .replace("Frase de ejemplo:", "\n\nFrase de ejemplo:")
+        .replace("Sinónimos:", "\n\nSinónimos:");
+
+        outputArray.push({
+          prompt: capitalizeFirstLetter(inputArray[i].say),
+          response: response,
+        });
     }
   }
 
   return outputArray;
 }
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
