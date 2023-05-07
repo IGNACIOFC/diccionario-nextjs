83f95828d03e1fe3fd38de209e3dc8f10db65506
Ignacio Fernandez <ifernandezcerro@gmail.com>
2023-05-08 00:38:40 +0200

    form modal

--- src/components/FormModal/FormModal.js
+++ src/components/FormModal/FormModal.js
@@ -1,13 +1,27 @@
+import React, { useState, useEffect } from "react";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-export default function FormModal ({ showModal, handleModal }) {
+export default function FormModal({ showModal, handleModal }) {
+  const [categories, setCategories] = useState([
+    { id: 1, name: "El dorado" },
+  ]);
+  const [showCategoryInput, setShowCategoryInput] = useState(false);
+
+  const handleCategoryChange = (e) => {
+    if (e.target.value === "new") {
+      setShowCategoryInput(true);
+    } else {
+      setShowCategoryInput(false);
+    }
+  };
+
   return (
     <>
       <Modal show={showModal} onHide={handleModal}>
         <Modal.Header closeButton>
-          <Modal.Title style={{color: "black"}}>Añadir palabra</Modal.Title>
+          <Modal.Title style={{ color: "black" }}>Añadir palabra</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
@@ -17,14 +31,35 @@ export default function FormModal ({ showModal, handleModal }) {
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Definición</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="Ingrese la definición" />
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="Ingrese la definición"
+              />
             </Form.Group>
             <Form.Group className="mb-1">
-              <Form.Select aria-label="Default select example">
+              <Form.Select
+                aria-label="Default select example"
+                onChange={handleCategoryChange}
+              >
                 <option>Guarda la palabra en una categoría</option>
-                <option value="1">El dorado</option>
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+                <option value="new">Nueva categoría</option>
               </Form.Select>
             </Form.Group>
+            {showCategoryInput && (
+              <Form.Group className="mb-3">
+                <Form.Label>Nueva categoría</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese el nombre de la nueva categoría"
+                />
+              </Form.Group>
+            )}
           </Form>
         </Modal.Body>
         <Modal.Footer>
@@ -39,3 +74,5 @@ export default function FormModal ({ showModal, handleModal }) {
     </>
   )
 }
+
+
